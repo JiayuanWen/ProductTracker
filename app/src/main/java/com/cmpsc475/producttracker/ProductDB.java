f39f1cd5353c4bef1e5dc5f97ed98d7207c30375
JiayuanWen <jbw5872@psu.edu>
2022-11-29 23:16:49 -0500

    Added Room Databse for products

---
+++ app/src/main/java/com/cmpsc475/producttracker/ProductDB.java
@@ -0,0 +1,9 @@
+package com.cmpsc475.producttracker;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Product.class}, version = 1)
+public abstract class ProductDB extends RoomDatabase {
+    public abstract ProductDao productDao();
+}
